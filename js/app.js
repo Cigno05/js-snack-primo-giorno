852f91eb11f98443f496f7927d40e97c109e7deb
Cigno05 <paolocalcagni.82@gmail.com>
2024-03-01 16:39:38 +0100

    Consegna esercizio

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// Esercizio 1
+// Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+
+const number = 100; //number
+
+console.log(number);
+
+// Esercizio 2
+// Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+
+let PI = 3.14; //number
+
+console.log(PI);
+
+// Esercizio 2/B
+// Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+
+PI = 3.1416; //number
+
+console.log(PI);
+
+// Esercizio 2/C
+// Dichiara una variabile chiamata radius e assegnale il valore 8.
+// Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+// Stampa il valore della variabile circle in console.
+
+let radius = 8; //number
+
+let circle = (radius * 2 * PI); //number
+
+console.log(circle);
+
+// Esercizio 3
+// Dichiara una variabile chiamata name e
+// assegnale il valore “Carlo”.
+// Successivamente, modifica il valore della variabile in “Marco”.
+// Stampa il nuovo valore in console.
+
+let name1 = 'Carlo'; //string
+
+name1 = 'Marco'; //string
+
+console.log(name1);
